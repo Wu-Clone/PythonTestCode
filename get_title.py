398e0d7d360f9d55845f84e97ba1f71c7a9034da
Wu-Clone <wuli77917@gmail.com>
2024-10-20 20:47:06 +0800

    爬虫获取标题

---
+++ get_title.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://e-hentai.org/s/7ca655a9cf/3096181-1"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+}
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.title.string if soup.title is not None else "No title found"
+    print(title)
+else:
+    print("无法获取，状态码：", response.status_code)
+
+
+clean_title = re.sub(r'[\/:*?"<>|]', ' ', title)
+if len(clean_title) > 50:
+    print(len(clean_title))
+    clean_title = clean_title[:50]
+print(clean_title)
